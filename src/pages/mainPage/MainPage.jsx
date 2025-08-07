92a19c89c1c7e0e0136b3a384582197abef9987a
a-neey <2371429@hansung.ac.kr>
2025-08-08 02:32:53 +0900

    ✨ : MainPage 구현

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,7 +1,11 @@
 import { Header } from "./components/Header"
+import { MenuPlus } from "./components/MenuPlus"
 
 export const MainPage = () => {
     return (
-        <Header />
+        <>
+            <Header />
+            <MenuPlus />        
+        </>
     )
 }
