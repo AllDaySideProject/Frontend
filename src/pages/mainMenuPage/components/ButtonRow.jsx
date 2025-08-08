79d10690fd8d8949fbc88d0ef88533c73985ccbe
a-neey <2371429@hansung.ac.kr>
2025-08-09 03:49:36 +0900

    ✨ : 버튼 onClick 네비게이션 기능 추가

--- src/pages/mainMenuPage/components/ButtonRow.jsx
+++ src/pages/mainMenuPage/components/ButtonRow.jsx
@@ -1,9 +1,23 @@
 import "./ButtonRow.scss";
 
+import { useNavigate } from "react-router-dom";
+
 import { ButtonComponent } from "../../../components/ButtonComponent"
 import ScreenContainer from "../../../components/ScreenContainer"
 
 export const ButtonRow = () => {
+    const navigate = useNavigate();
+
+    const handleMoreClick = () => {
+        navigate(``);
+        console.log("내 밥상 추가로 이동");
+    }
+
+    const handleBookingClick = () => {
+        navigate(``);
+        console.log("픽업 예약으로 이동");
+    }
+
     return (
         <ScreenContainer>
             <div className = "buttonContainer">
@@ -13,6 +27,7 @@ export const ButtonRow = () => {
                     buttonText = "더 추가하기"
                     textSize = { '0.88rem' }
                     textColor = { '#B0B0B0' }
+                    onClick = { handleMoreClick }
                 />
                 <ButtonComponent
                     width = { '11.88rem' }
@@ -20,6 +35,7 @@ export const ButtonRow = () => {
                     buttonText = "픽업 예약하기"
                     textSize = { '0.88rem' }
                     textColor = { '#FFFFFF' }
+                    onClick = { handleBookingClick }
                 />
             </div>  
     </ScreenContainer>
