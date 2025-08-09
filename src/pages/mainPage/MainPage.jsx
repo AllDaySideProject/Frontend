538681cb4ae327f36ec6c936a62b9f9507fe1e31
a-neey <2371429@hansung.ac.kr>
2025-08-10 01:12:37 +0900

    ⚰️ : 사용하지 않는 임포트 삭제

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,6 +1,4 @@
-import { DeleteModal } from "../../components/DeleteModal"
 import { Header } from "./components/Header"
-import { MenuBox } from "../mainMenuPage/components/MenuBox"
 import { MenuPlus } from "./components/MenuPlus"
 
 export const MainPage = () => {
