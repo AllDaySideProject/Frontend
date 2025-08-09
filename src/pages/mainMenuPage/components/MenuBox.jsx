838f430f0df6421b03aaed8c21328f8b9c9da178
a-neey <2371429@hansung.ac.kr>
2025-08-09 22:51:34 +0900

    ✨ : MenuBox 모드에 따른 조건부 렌더링 기능 추가

--- src/pages/mainMenuPage/components/MenuBox.jsx
+++ src/pages/mainMenuPage/components/MenuBox.jsx
@@ -1,15 +1,52 @@
 import "./MenuBox.scss";
 import LOCATION from "../../../assets/main/storeLocation.svg";
+import CHECKBOX from "../../../assets/main/checkbox.svg";
 
-export const MenuBox = ({ menuName, storeName, count, price }) => {
+export const MenuBox = ({ mode = "normal", isSelected = false, onToggleSelect, menuName, storeName, count, price }) => {
+    const isDelete = mode === "delete";
+
+    if (isDelete) {
+        return (
+            <div className = "menuBoxContainer deleteMode">
+                <img className = "menuImage" />
+                <div className = "menuContainer">
+                    <div className = "infoBox">
+                        <div className = "menuInfo">
+                            <p className = "priceText">{ price }원</p>
+                            <p className = "section">l</p>
+                            <div className = "priceInfo">
+                                <p>{ menuName } { count }개</p>
+                            </div>
+                        </div>    
+                        <div className = "storeInfo">
+                            <img 
+                                src = { LOCATION } 
+                                className = "locationImage"
+                            />
+                            <p>{ storeName }</p>
+                        </div>    
+                    </div>
+                    <img 
+                        src = { CHECKBOX }
+                        className = "checkBoxImage"
+                    />       
+                </div> 
+            </div>
+
+        )
+    }
+
     return (
-        <div className = "menuBoxContainer">
+        <div className = "menuBoxContainer normalMode">
             <img className = "menuImage" />
             <div className = "infoBox">
                 <div className = "menuInfo">
                     <p className = "menuName">{ menuName }</p>
                     <div className = "storeInfo">
-                        <img src = { LOCATION } />
+                        <img 
+                            src = { LOCATION } 
+                            className = "locationImage"
+                        />
                         <p>{ storeName }</p>
                     </div>
                 </div>
