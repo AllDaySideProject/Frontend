551cd7768ae1b112edab466c9e0b683267f38aba
a-neey <2371429@hansung.ac.kr>
2025-08-09 18:19:31 +0900

    💄 : 버튼 비활성화 시 스타일 추가

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -7,7 +7,7 @@ export const ButtonComponent = ({ width, bgColor, onClick, textSize, textColor, 
             className = "buttonBox"
             style = {{ 
                 width: width,
-                backgroundColor: bgColor,
+                backgroundColor: disabled ? "#B0B0B0" : bgColor,
                 pointerEvents: disabled ? "none" : "auto"
             }}
             onClick = { disabled ? undefined : onClick } // 버튼 비활성화 시 onClick: undefined
