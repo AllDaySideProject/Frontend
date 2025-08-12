8486e10203be6d6b182b4a790a26b8e4370cd4a6
a-neey <2371429@hansung.ac.kr>
2025-08-13 01:50:59 +0900

    ✨ : Location 컴포넌트 생성

---
+++ src/pages/pickupPage/components/Location.jsx
@@ -0,0 +1,13 @@
+import "./Location.scss";
+
+import ScreenContainer from "../../../components/ScreenContainer"
+
+export const Location = () => {
+    return (
+        <ScreenContainer>
+            <div className = "locationContainer">
+                지도 화면
+            </div>
+        </ScreenContainer>
+    )
+}
