18f3908943b351136761fb6fb4df16292ff95ede
a-neey <2371429@hansung.ac.kr>
2025-08-13 01:52:12 +0900

    ✨ : PickupPage 조건부 렌더링

--- src/pages/pickupPage/PickupPage.jsx
+++ src/pages/pickupPage/PickupPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { HeaderArrow } from "../../components/HeaderArrow";
 import { PickupIntro } from "./components/PickupIntro";
 import { PickupState } from "./components/PickupState";
+import { Code } from "./components/Code";
+import { Location } from "./components/Location";
 
 export const PickupPage = () => {
     const [view, setView] = useState("location");
@@ -11,6 +13,8 @@ export const PickupPage = () => {
             <HeaderArrow />
             <PickupIntro view = { view } />
             <PickupState view = { view } onChange = { setView } />
+
+            { view === "location" ? <Location /> : <Code /> }     
         </> 
     )
 }
