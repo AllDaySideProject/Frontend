64d3f23a7df19b38642712f03e0f1addb067b0f7
a-neey <2371429@hansung.ac.kr>
2025-08-13 01:58:58 +0900

    ♻️ : 코드 배열로 변경

--- src/pages/pickupPage/components/Code.jsx
+++ src/pages/pickupPage/components/Code.jsx
@@ -5,6 +5,8 @@ import { ButtonComponent } from "../../../components/ButtonComponent";
 import { useState } from "react";
 
 export const Code = () => {
+    const code = "JF0527";
+    
     const [pressed, setPressed] = useState(false);
 
     const handleEndCick = () => {
@@ -14,7 +16,7 @@ export const Code = () => {
     return (
         <ScreenContainer>
             <div className = "codeContainer">
-                <p className = "codeText">JF0527</p>
+                <p className = "codeText">{ code }</p>
                 <p className = "description">
                     매장에 방문 후 계산대(카운터) 또는 지정된 픽업 장소에서<br />
                     직원의 안내에 따라 위의 픽업 코드를 보여 주시면<br />
