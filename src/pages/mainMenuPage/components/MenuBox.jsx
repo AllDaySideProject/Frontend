c8aa1d3cc931dd2de8666180f7730941f94e04d9
a-neey <2371429@hansung.ac.kr>
2025-08-12 13:21:51 +0900

    ✨ : MenuBox 컴포넌트 삭제 모드 추가

--- src/pages/mainMenuPage/components/MenuBox.jsx
+++ src/pages/mainMenuPage/components/MenuBox.jsx
@@ -14,43 +14,43 @@ export const MenuBox = ({ mode = "normal", isSelected = false, onToggleSelect, m
     );
 
     const unitDiscountRate = unitOriginal > 0 ? Math.round(((unitOriginal - price) / unitOriginal) * 100) : 0;
-    // if (isDelete) {
-    //     return (
-    //         <div className = "menuBoxContainer deleteMode">
-    //             <img 
-    //                 // alt = "메뉴 아이콘"
-    //                 // src = { 추가 에정 }
-    //                 className = "menuImage"
-    //             />
-    //             <div className = "menuContainer">
-    //                 <div className = "infoBox">
-    //                     <div className = "menuInfo">
-    //                         <p className = "priceText">{ fmt(price) }원</p>
-    //                         <p className = "section">l</p>
-    //                         <div className = "priceInfo">
-    //                             <p>{ menuName } { count }개</p>
-    //                         </div>
-    //                     </div>    
-    //                     <div className = "storeInfo">
-    //                         <img 
-    //                             alt = "위치 아이콘"
-    //                             src = { LOCATION } 
-    //                             className = "locationImage"
-    //                         />
-    //                         <p>{ storeName }</p>
-    //                     </div>    
-    //                 </div>
-    //                 <img 
-    //                     src = { isSelected ? CHECKEDBOX : CHECKBOX } 
-    //                     className = "checkBoxImage"
-    //                     alt = { isSelected ? "선택 완료" : "미선택" }
-    //                     onClick = { (e) => onToggleSelect?.() }
-    //                 />       
-    //             </div> 
-    //         </div>
+    
+    if (isDelete) {
+        return (
+            <div className = "deleteContainer">
+                <div className = "menuBoxContainer deleteMode">
+                    <div className = "menuBoxLeft">
+                        <img 
+                            // alt = "메뉴 아이콘"
+                            className = "menuImage" 
+                        />
+                        <div className = "infoBox">
+                            <div className = "menuInfo">
+                                <p className = "storeName">{ storeName }</p>
+                                <p className = "menuName">{ menuName }</p>
+                                <p className = "menuCount">수량: { count }개</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className = "menuBoxRight">
+                        <div className = "originText">
+                            { discount > 0 && <p className = "originalPrice">{ fmt(unitOriginal) }</p> }
+                            <p className = "originWon">원</p>                    
+                        </div>
+                        <div className = "finalInfo">
+                            { discount > 0 && <p className = "discountRate">-{ unitDiscountRate }%</p> }
+                            <p className = "finalPrice">{ fmt(price * count) }원</p>                    
+                        </div>
+                    </div>
+                </div>  
 
-    //     )
-    // }
+                <img 
+                    src = { isSelected ? CHECKEDBOX : CHECKBOX }
+                    onClick = { () => onToggleSelect?.() }
+                />
+            </div>
+        )
+    }
 
     return (
         <div className = "menuBoxContainer normalMode">
@@ -67,7 +67,6 @@ export const MenuBox = ({ mode = "normal", isSelected = false, onToggleSelect, m
                     </div>
                 </div>
             </div>
-
             <div className = "menuBoxRight">
                 <div className = "originText">
                     { discount > 0 && <p className = "originalPrice">{ fmt(unitOriginal) }</p> }
@@ -77,7 +76,6 @@ export const MenuBox = ({ mode = "normal", isSelected = false, onToggleSelect, m
                     { discount > 0 && <p className = "discountRate">-{ unitDiscountRate }%</p> }
                     <p className = "finalPrice">{ fmt(price * count) }원</p>                    
                 </div>
-
             </div>
         </div>
     )
