b4aaf07778a3ef8da677eb01593108ba3334ccc2
a-neey <2371429@hansung.ac.kr>
2025-08-13 22:43:06 +0900

    💄 : 메인 화면 배경 이미지 수정

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -5,30 +5,21 @@ import { useLocationPermission } from "../../components/LocationPermissionContex
 import { Header } from "./components/Header"
 import { MenuPlus } from "./components/MenuPlus"
 
-import BGIMG from "../../assets/main/bgImage.svg";
-
 export const MainPage = () => {
     const { decided, requestLocation} = useLocationPermission();
 
-    useEffect(() => { // 최초 접속 또는 사용자가 결정하지 않았을 때 권한 요청
+    useEffect(() => {
         if (!decided) requestLocation();
     }, [decided, requestLocation]);
 
     return (
         <>
             <Header />
-            <div 
-                className = "mainBg"
-                style = {{ 
-                    backgroundImage: `url(${ BGIMG })`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    backgroundRepeat: "no-repeat",
-                    minHeight: "100vh",
-                }}
-            >
-                <MenuPlus />
+            <div className="mainBg">
+                <div className="mainBgInner">
+                    <MenuPlus />
+                </div>
             </div>
         </>
-    )
-}
+    );
+};
