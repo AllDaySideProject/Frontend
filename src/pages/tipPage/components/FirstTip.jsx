38aae82f0f4f5564d916c0270d403b67ad300109
a-neey <2371429@hansung.ac.kr>
2025-08-13 11:43:09 +0900

    ✨ : FirstTip 컴포넌트 생성

--- src/pages/tipPage/components/FirstTip.jsx
+++ src/pages/tipPage/components/FirstTip.jsx
@@ -1,9 +1,20 @@
+import "./FirstTip.scss";
+
 import ScreenContainer from "../../../components/ScreenContainer"
+
+import TIP1 from "../../../assets/tips/tip-1.svg";
 
 export const FirstTip = () => {
     return (
-        <ScreenContainer>
-            
-        </ScreenContainer>
+        <div className = "firstContainer">
+            <div className = "tipText">
+                <p className = "pageTitle">AI 알뜰식사 TIP</p>
+                <p className = "titleText">
+                    내 자취 라이프,<br />
+                    내 맘대로 즐기는 알뜰 한 끼
+                </p>
+            </div>
+            <img src = { TIP1 } />
+        </div>
     )
 }
