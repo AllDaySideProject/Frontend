08ea312c78a7af521c7eac8cb1c7bcf5dc845186
a-neey <2371429@hansung.ac.kr>
2025-08-13 12:04:29 +0900

    ✏️ : ThirdTip 문구 수정

--- src/pages/tipPage/components/ThirdTip.jsx
+++ src/pages/tipPage/components/ThirdTip.jsx
@@ -9,13 +9,13 @@ export const ThirdTip = () => {
         <div className = "thirdContainer">
             <div className = "tipText">
                 <p className = "tipTitle">
-                    두부조림은<br />
-                    자투리 채소 활용에 좋아요
+                    버섯볶음,<br />
+                    든든한 한 그릇 덮밥으로!
                 </p>
                 <p className = "tipDescription">
-                    양파, 대파, 청양고추, 느타리버섯 등<br />
-                    냉장고에 애매하게 남아 있는 자투리 채소들이 있다면<br />
-                    두부와 함께 푸짐한 조림으로 변신시켜 보세요!
+                    따뜻한 밥 위에 남은 버섯볶음 듬뿍 얹어 주세요.<br />
+                    참기름 살짝 두르고, 김가루나 깨를 솔솔 뿌리면<br />
+                    근사한 버섯덮밥이 뚝딱 완성돼요!
                 </p>
             </div>
             <div className = "thirdImgBox">
