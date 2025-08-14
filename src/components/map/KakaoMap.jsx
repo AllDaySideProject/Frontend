5752940154e8ee30a1aeb048629475755109366b
a-neey <2371429@hansung.ac.kr>
2025-08-14 11:39:36 +0900

    ✨ : 지도 표시

--- src/components/map/KakaoMap.jsx
+++ src/components/map/KakaoMap.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Map } from "react-kakao-maps-sdk";
 
 function KakaoMap(props) {
+  const userPos = { lat: 37.5820, lng: 127.0104 };
+  const [stores, setStores] = useState([
+    { lat: 37.5700, lng: 127.0204 },
+    { lat: 37.5900, lng: 127.0164 },
+    { lat: 37.5970, lng: 127.0064 }
+  ]);
+
+  const allPoints = [userPos, ...stores]; // 모든 좌표 배열
+  const centerPos = { // 좌표 중앙 계산
+    lat: allPoints.reduce((sum, p) => sum + p.lat, 0) / allPoints.length,
+    lng: allPoints.reduce((sum, p) => sum + p.lng, 0) / allPoints.length,
+  };
+
   return (
     <Map
-      center = {{ lat: 37.46829, lng: 127.03907 }}
-      style = {{ width: '1000px', height: '600px' }}
-      level = { 3 }
-    />  
+      center = { centerPos }
+
+      style = {{ width: '24.375rem', height: '50rem' }}
+      level = { 5 }
+
+      onCreate = {(map) => {
+        const { kakao } = window;
+        const bounds = new kakao.maps.LatLngBounds();
+
+        bounds.extend(new kakao.maps.LatLng(userPos.lat, userPos.lng)); // 사용자 위치
+
+        stores.forEach(store => {
+          bounds.extend(new kakao.maps.LatLng(store.lat, store.lng));
+        })
+
+        map.setBounds(bounds);
+      }}
+    />
   );
 }
 
