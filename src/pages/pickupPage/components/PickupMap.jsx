bffa05d4e0a4557333f34e4c05e90198505df418
a-neey <2371429@hansung.ac.kr>
2025-08-15 04:19:12 +0900

    ✨ : PickupMap 컴포넌트 생성

---
+++ src/pages/pickupPage/components/PickupMap.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { MapMarker, Polyline } from "react-kakao-maps-sdk";
+import { BaseKakaoMap } from "../../../components/map/BaseKakaoMap";
+import USER from "../../../assets/map/userLocation.svg";
+import STORE_GR from "../../../assets/map/storeLocation-green.svg";
+import STORE_WH from "../../../assets/map/storeLocation-white.svg";
+
+export default function PickupMap({ userPos, destinations = [], paths = [], height = "50rem", width, useBent = false, selectedId, onDestinationClick }) {
+  if (!userPos) return null;
+
+  const boundsPoints = [  // 사용자 위치 + 모든 목적지 + 모든 경로 좌표
+    userPos,
+    ...destinations,
+    ...paths.flat()
+  ];
+
+  return (
+    <BaseKakaoMap 
+      center = { userPos}  
+      boundsPoints = { boundsPoints }  
+      height = { height } 
+      width = { width }
+    >
+      
+      <MapMarker // 사용자 마커
+        position = { userPos }
+        image = {{
+          src: USER,
+          size: { width: 50, height: 50 },
+          options: { offset: { x: 25, y: 25 } }
+        }}
+      />
+
+      { destinations.map(dest => { // 목적지 마커
+        const isSelected = dest.id === selectedId;
+        return (
+          <MapMarker
+            key = { dest.id }
+            position = {{ lat: dest.lat, lng: dest.lng }}
+            image = {{
+              src: isSelected ? STORE_GR : STORE_WH,
+              size: { width: 40, height: 40 },
+              options: { offset: { x: 20, y: 20 } }
+            }}
+            onClick = { () => onDestinationClick?.(dest.id) }
+          />          
+        )
+      })}
+
+      { paths.map((path, idx) => ( // 목적지 있을 때 경로 표시
+        <Polyline
+          key = { idx }
+          path = { path }
+          strokeWeight = { 2 }
+          strokeColor = "#0EA64B"
+          strokeOpacity = { 1 }
+          strokeStyle = "dash"
+        />
+      ))}
+
+    </BaseKakaoMap>
+  );
+}
