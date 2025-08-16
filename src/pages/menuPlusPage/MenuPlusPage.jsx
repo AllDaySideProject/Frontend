fbec5c5768bed9d75e8e78a5b3db6d055ce5d270
a-neey <2371429@hansung.ac.kr>
2025-08-17 01:24:13 +0900

    💄 : MenuPlusPage 여백 색상 수정

--- src/pages/menuPlusPage/MenuPlusPage.jsx
+++ src/pages/menuPlusPage/MenuPlusPage.jsx
@@ -1,9 +1,12 @@
+import "./MenuPlusPage.scss";
 import MenuPlusMap from "./components/MenuPlusMap"
 
 export const MenuPlusPage = () => {
     return (
-        <div style = {{ display: "flex", justifyContent: "center", height: "100vh" }}>
-            <MenuPlusMap />
+        <div className = "mapBg">
+            <div className = "mapInner">
+                <MenuPlusMap />
+            </div>
         </div>
     )
 }
