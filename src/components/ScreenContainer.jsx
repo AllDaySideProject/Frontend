bfffe88e128fd387805ff5b3dc6dd5e8715e2bff
a-neey <2371429@hansung.ac.kr>
2025-08-17 00:52:48 +0900

    💄 : 메인 화면 여백 색상 수정

--- src/components/ScreenContainer.jsx
+++ src/components/ScreenContainer.jsx
@@ -1,12 +1,12 @@
 import "./ScreenContainer.scss";
 
-const ScreenContainer = ({ children, bgColor = "#FAFAFA" }) => {
+const ScreenContainer = ({ children, bgColor = "#FAFAFA", padding = "2rem" }) => {
     return (
 
     <div className="screenContainer">
       <div 
         className="screenContent"
-        style = {{ backgroundColor: bgColor }}
+        style = {{ backgroundColor: bgColor, paddingLeft: padding, paddingRight: padding }}
       >{children}
       </div>
     </div>
