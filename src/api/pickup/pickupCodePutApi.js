99a5512fe2b5fb464d80cf42f5dc01c7b43c9ab2
a-neey <2371429@hansung.ac.kr>
2025-08-18 02:41:54 +0900

    ✨ : pickupCodePutApi 생성

---
+++ src/api/pickup/pickupCodePutApi.js
@@ -0,0 +1,38 @@
+import { defaultInstance } from "../utils/defaultInstance";
+
+export const pickupCodePutApi = async (code) => {
+  try {
+    const response = await defaultInstance.put(`/orders/complete`, { code });
+
+    if (response.status === 200) {
+      console.log("코드 호출 성공:", response.data.message);
+    }
+
+    return response.data;
+  } catch (error) {
+    const status = error.response?.status; 
+    const data = error.response?.data;   
+
+    switch (status) {
+      case 400:
+            if (data?.code === "GLOBAL_400_2")
+                console.error("잘못된 요청: ", data?.message);
+            else if (data?.code === "ORDER_400_1")
+                console.error("잘못된 요청: ", data?.message);
+        break;
+
+      case 404:
+        console.error("주문을 찾을 수 없음: ", data?.message);
+        break;
+
+      case 500:
+        console.error("서버 오류: ", data?.message);
+        break;
+
+      default:
+        console.error("알 수 없는 오류: ", data?.message ?? error.message);
+    }
+
+    throw error;
+  }
+};
